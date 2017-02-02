eff4b0ba55d41072d80522f9ddff2b7c0211ebf9
pizajoan <pizajoan97@gmail.com>
2017-02-02 18:03:47 +0100

    Segon, equals ok encara "pareix", toString ok "pareix", constructor a partir de String s'ha de fer amb array etc.

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -19,6 +19,29 @@ public class Polynomial {
 
     // Constructor a partir d'un string
     public Polynomial(String s) {
+        String auxiliar;
+        int grau = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '^') {
+                grau = (int) s.charAt(i+1);
+            }
+        }
+        float [] pol = new float[grau+1];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                if (s.charAt(i) < 0 || s.charAt(i) > 0) {
+                    if (i > 0 && s.charAt(i-1) != '^') {
+                        if (s.charAt(i) > 0) {
+                            pol[i] = s.charAt(i);
+                        }
+                        if (s.charAt(i) < 0)
+                            pol[i] = s.charAt(i)*-1;
+                    }
+                }
+            }
+        }
+        this.nums = pol;
+
     }
 
     // Suma el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
@@ -45,8 +68,8 @@ public class Polynomial {
     // Torna "true" si els polinomis són iguals. Això és un override d'un mètode de la classe Object
     @Override
     public boolean equals(Object o) {
-        Polynomial x = (Polynomial) o;
-        return x.toString().equals(this.toString());
+        Polynomial p = (Polynomial) o;
+        return p.toString().equals(this.toString());
     }
 
     // Torna la representació en forma de String del polinomi. Override d'un mètode de la classe Object
@@ -54,6 +77,10 @@ public class Polynomial {
     public String toString() {
         StringBuilder sb =  new StringBuilder();
         for (int i = 0; i < this.nums.length; i++) {
+            if (this.nums[i] == 0 && i == 0) {
+                sb.append((int) this.nums[i]);
+                continue;
+            }
             if (this.nums.length == 1) {
                 if (this.nums[i] == 0 )  {
                     sb.append((int) this.nums[i]);
@@ -63,43 +90,52 @@ public class Polynomial {
                     sb.append((int) this.nums[i]);
                 }
             }
-                if (this.nums[i] == 0 && i == 0) {
-                    sb.append((int) this.nums[i]);
-                    continue;
-                }
                 if (this.nums.length == 2) {
                     if (this.nums[i] == 1) {
                         sb.append("x");
+                        continue;
                     }
-                    if (this.nums[i] != 1 && this.nums[i] > 0) {
+                    if (this.nums[i] != 1 && i == 0) {
+                        sb.append((int) this.nums[i] + "x");
+                        continue;
+                    }
+                    if (this.nums[i] != 1 && this.nums[i] != 0 && this.nums[i] > 0) {
                         sb.append(" + " + (int) this.nums[i]);
+                        continue;
                     }
-                    if (this.nums[i] != 1 && this.nums[i] < 0) {
+                    if (this.nums[i] != 1 && this.nums[i] != 0 && this.nums[i] < 0) {
                         sb.append(" - " + (int) this.nums[i]*-1);
+                        continue;
                     }
                 }
             if (this.nums.length > 2) {
-                if (this.nums[i] != 0 && this.nums.length-i == this.nums.length) {
+                if (this.nums[i] != 0 && this.nums[i] != 1 && this.nums.length-i == this.nums.length) {
                     sb.append((int) this.nums[i] + "x^" + (this.nums.length-1-i));
                     continue;
                 }
-                if (this.nums[i] != 0 && this.nums.length-i-1 > 1 && this.nums[i] > 0) {
+                if (this.nums[i] != 0 && this.nums[i] != 1 && this.nums.length-i-1 > 1 && this.nums[i] > 0) {
                     sb.append(" + " + (int) this.nums[i] + "x^" + (this.nums.length-1-i));
+                    continue;
                 }
-                if (this.nums[i] != 0 && this.nums.length-i-1 > 1 && this.nums[i] < 0) {
+                if (this.nums[i] != 0 && this.nums[i] != 1 && this.nums.length-i-1 > 1 && this.nums[i] < 0) {
                     sb.append(" - " + (int) this.nums[i]*-1 + "x^" + (this.nums.length-1-i));
+                    continue;
                 }
-                if (this.nums[i] != 0 && this.nums.length-1-i == 1 && this.nums[i] > 0) {
+                if (this.nums[i] != 0 && this.nums[i] != 1 && this.nums.length-1-i == 1 && this.nums[i] > 0) {
                     sb.append(" + " + (int) this.nums[i] + "x");
+                    continue;
                 }
-                if (this.nums[i] != 0 && this.nums.length-1-i == 1 && this.nums[i] < 0) {
+                if (this.nums[i] != 0 && this.nums[i] != 1 && this.nums.length-1-i == 1 && this.nums[i] < 0) {
                     sb.append(" - " + (int) this.nums[i]*-1 + "x");
+                    continue;
                 }
-                if (this.nums[i] != 0 && this.nums.length-1-i == 0 && this.nums[i] < 0) {
+                if (this.nums[i] != 0 && this.nums[i] != 1 && this.nums.length-1-i == 0 && this.nums[i] < 0) {
                     sb.append(" - " + (int) this.nums[i]*-1);
+                    continue;
                 }
-                if (this.nums[i] != 0 && this.nums.length-1-i == 0 && this.nums[i] > 0) {
-                    sb.append(" + " + (int) this.nums[i]*-1);
+                if (this.nums[i] != 0 && this.nums[i] != 1 && this.nums.length-1-i == 0 && this.nums[i] > 0) {
+                    sb.append(" + " + (int) this.nums[i]);
+                    continue;
                 }
             }
         }
